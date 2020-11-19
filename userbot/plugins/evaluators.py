35ff1785a62801187067150a2b420821e956470f
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-11-19 23:35:51 +0530

    Update evaluators.py

--- userbot/plugins/evaluators.py
+++ userbot/plugins/evaluators.py
@@ -1,96 +1,51 @@
-"""Execute GNU/Linux commands inside Telegram
-Syntax: .exec Code"""
+# Execute GNU/Linux commands inside Telegram
+
 import asyncio
 import io
 import sys
 import traceback
-
+import os
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 from . import *
 
 
-@bot.on(admin_cmd(pattern="bash (.*)"))
-@bot.on(sudo_cmd(pattern="bash (.*)", allow_sudo=True))
+@bot.on(admin_cmd(pattern="exec(?: |$|\n)(.*)"))
+@bot.on(sudo_cmd(pattern="exec(?: |$|\n)(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
-    if event.fwd_from or event.via_bot_id:
-        return
-    cmd = "".join(event.text.split(maxsplit=1)[1:])
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
+    cmd = event.pattern_match.group(1)
+    catevent = await edit_or_reply(event, "`Executing.....`")
     process = await asyncio.create_subprocess_shell(
         cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
     stdout, stderr = await process.communicate()
-    OUTPUT = f"`{stdout.decode()}`"
-    if len(OUTPUT) > Config.MAX_MESSAGE_SIZE_LIMIT:
-        with io.BytesIO(str.encode(OUTPUT)) as out_file:
-            out_file.name = "bash.text"
-            await event.client.send_file(
-                event.chat_id,
-                out_file,
-                force_document=True,
-                allow_cache=False,
-                caption=cmd,
-                reply_to=reply_to_id,
-            )
-            await event.delete()
+    result = str(stdout.decode().strip()) + str(stderr.decode().strip())
+    catuser = await event.client.get_me()
+    if catuser.username:
+        curruser = catuser.username
     else:
-        await edit_or_reply(event, OUTPUT)
+        curruser = "catuserbot"
+    uid = os.geteuid()
+    if uid == 0:
+        cresult = f"`{curruser}:~#` `{cmd}`\n`{result}`"
+    else:
+        cresult = f"`{curruser}:~$` `{cmd}`\n`{result}`" 
+    await edit_or_reply(catevent ,text=cresult, aslink=True, linktext=f"**•  Exec : **\n`{cmd}` \n\n**•  Result : **\n")
+    if BOTLOG:
+        await event.client.send_message(
+            BOTLOG_CHATID,
+            "Terminal command " + command + " was executed sucessfully.",
+        )
 
 
-@bot.on(admin_cmd(pattern="exec (.*)"))
-@bot.on(sudo_cmd(pattern="exec (.*)", allow_sudo=True))
+@bot.on(admin_cmd(pattern="eval(?: |$|\n)(.*)"))
+@bot.on(sudo_cmd(pattern="eval(?: |$|\n)(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
-    if event.fwd_from or event.via_bot_id:
-        return
-    cmd = "".join(event.text.split(maxsplit=1)[1:])
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-    process = await asyncio.create_subprocess_shell(
-        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-    )
-    stdout, stderr = await process.communicate()
-    e = stderr.decode()
-    if not e:
-        e = "No Error"
-    o = stdout.decode()
-    if not o:
-        o = "**Tip**: \n`If you want to see the results of your code, I suggest printing them to stdout.`"
-    else:
-        _o = o.split("\n")
-        o = "`\n".join(_o)
-    OUTPUT = f"**QUERY:**\n__Command:__\n`{cmd}` \n__PID:__\n`{process.pid}`\n\n**stderr:** \n`{e}`\n**Output:**\n{o}"
-    if len(OUTPUT) > Config.MAX_MESSAGE_SIZE_LIMIT:
-        with io.BytesIO(str.encode(OUTPUT)) as out_file:
-            out_file.name = "exec.text"
-            await event.client.send_file(
-                event.chat_id,
-                out_file,
-                force_document=True,
-                allow_cache=False,
-                caption=cmd,
-                reply_to=reply_to_id,
-            )
-            await event.delete()
-    else:
-        await edit_or_reply(event, OUTPUT)
-
-
-@bot.on(admin_cmd(pattern="eval (.*)"))
-@bot.on(sudo_cmd(pattern="eval (.*)", allow_sudo=True))
-async def _(event):
-    if event.fwd_from:
-        return
-    if event.via_bot_id:
-        return
-    cmd = event.text.split(" ", maxsplit=1)[1]
-    catevent = await edit_or_reply(event, "Processing ...")
+    cmd = event.pattern_match.group(1)
+    catevent = await edit_or_reply(event, "`Running ...`")
     old_stderr = sys.stderr
     old_stdout = sys.stdout
     redirected_output = sys.stdout = io.StringIO()
@@ -115,6 +70,11 @@ async def _(event):
         evaluation = "Success"
     final_output = f"**•  Eval : **\n`{cmd}` \n\n**•  Result : **\n`{evaluation}` \n"
     await edit_or_reply(catevent ,text=final_output, aslink=True, linktext=f"**•  Eval : **\n`{cmd}` \n\n**•  Result : **\n")
+    if BOTLOG:
+        await event.client.send_message(
+            BOTLOG_CHATID,
+            "eval command " + cmd + " was executed sucessfully.",
+        )
 
 
 async def aexec(code, smessatatus):
