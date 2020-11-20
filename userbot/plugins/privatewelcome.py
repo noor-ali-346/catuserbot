4790412eb56734b75c40744b84da27549bda7048
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-11-20 18:23:48 +0530

    Update privatewelcome.py

--- userbot/plugins/privatewelcome.py
+++ userbot/plugins/privatewelcome.py
@@ -1,6 +1,6 @@
 from telethon import events
 
-from .. import BOTLOG_CHATID, CMD_HELP, bot
+from . import BOTLOG_CHATID, CMD_HELP, bot
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 from .sql_helper import pmpermit_sql as pmpermit_sql
 from .sql_helper.welcomesql import (addwelcome_setting,
