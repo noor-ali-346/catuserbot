acbe9254b54c58ed893aef91700bdd88f6136111
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-11-20 17:33:30 +0000

    pylint: auto fixes

--- userbot/plugins/autoprofile.py
+++ userbot/plugins/autoprofile.py
@@ -17,8 +17,8 @@ from pySmartDL import SmartDL
 from telethon.errors import FloodWaitError
 from telethon.tl import functions
 
-from . import AUTONAME, CMD_HELP, DEFAULT_BIO
 from ..utils import admin_cmd
+from . import AUTONAME, CMD_HELP, DEFAULT_BIO
 
 DEFAULTUSERBIO = str(DEFAULT_BIO) if DEFAULT_BIO else " ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ  "
 CHANGE_TIME = Config.CHANGE_TIME
